6a2d2a563b0d64ee0e03c2298fe1e89b2415035b
rosswmerrifield <rosswmerrifield@gmail.com>
2013-08-06 08:37:22 -0700

    Update static.py

--- static.py
+++ static.py
@@ -2,7 +2,7 @@
 
 import subprocess
 
-int = raw_input("Interface:\n> ")
+intr = raw_input("Interface:\n> ")
 addr = raw_input("IP Address:\n> ")
 mask = raw_input("Subnet Mask:\n> ")
 gate = raw_input("Default Gateway (optional):\n> ")
@@ -10,18 +10,18 @@ dns = raw_input("DNS Server (optional):\n> ")
 
 subprocess.call(["sudo", "cp", "/etc/resolv.conf", "/tmp/resolv.conf.backup"])
 
-if int == "en0":
+if intr == "en0":
 	subprocess.call(["sudo", "networksetup", "-setmanual", "ethernet", addr, mask, gate])
-elif int == "en1":
+elif intr == "en1":
 	subprocess.call(["sudo", "networksetup", "-setmanual", "Wi-Fi", addr, mask, gate])
 else:
 	print "no valid interface specified (en0 or en1)"
 	quit()
 
 if len(dns) != 0:
-	if int=="en0":
+	if intr=="en0":
 		subprocess.call(["sudo", "networksetup", "-setdnsservers", "ethernet", dns])
-	elif int=="en1":
+	elif intr=="en1":
 		subprocess.call(["sudo", "networksetup", "-setdnsservers", "Wi-FI", dns])
 else:
 	subprocess.call(["sudo", "mv", "/tmp/resolv.conf.backup", "/etc/resolv.conf"])
