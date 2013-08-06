bd4f8f19b859f6afbc1353fb8c26ce000f336046
rosswmerrifield <rosswmerrifield@gmail.com>
2013-08-06 08:36:44 -0700

    Update static.py

--- static.py
+++ static.py
@@ -8,6 +8,8 @@ mask = raw_input("Subnet Mask:\n> ")
 gate = raw_input("Default Gateway (optional):\n> ")
 dns = raw_input("DNS Server (optional):\n> ")
 
+subprocess.call(["sudo", "cp", "/etc/resolv.conf", "/tmp/resolv.conf.backup"])
+
 if int == "en0":
 	subprocess.call(["sudo", "networksetup", "-setmanual", "ethernet", addr, mask, gate])
 elif int == "en1":
@@ -22,6 +24,6 @@ if len(dns) != 0:
 	elif int=="en1":
 		subprocess.call(["sudo", "networksetup", "-setdnsservers", "Wi-FI", dns])
 else:
-	pass	
+	subprocess.call(["sudo", "mv", "/tmp/resolv.conf.backup", "/etc/resolv.conf"])
 
 quit()
